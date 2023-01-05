60dfb52fb6ec35f207eca0735936d46ff6a84698
vpvm96 <vpvm96@gmail.com>
2023-01-05 11:30:55 +0900

    feat. cleanupcode 를 위한 폴더분해

---
+++ api.js
@@ -0,0 +1,24 @@
+const BASE_URL = "https://api.themoviedb.org/3/movie"
+const API_KEY = "6bddfa41d1886e777ac198dc0c085925"
+
+export const getNowPlaying = () =>
+  fetch(
+    `${BASE_URL}/now_playing?api_key=${API_KEY}&language=en-US&page=1`
+  ).then((res) => res.json())
+
+export const getTopReated = ({ pageParam = 1 }) =>
+  fetch(
+    `${BASE_URL}/top_rated?api_key=${API_KEY}&language=en-US&page=${pageParam}`
+  ).then((res) => res.json())
+
+export const getUpcoming = ({ pageParam = 1 }) =>
+  fetch(
+    `${BASE_URL}/upcoming?api_key=${API_KEY}&language=en-US&page=${pageParam}`
+  ).then((res) => res.json())
+
+export const getDetail = async (params) => {
+  const [_, movieId] = params.queryKey
+  return fetch(
+    `${BASE_URL}/${movieId}?api_key=${API_KEY}&language=en-US&append_to_response=videos`
+  ).then((res) => res.json())
+}
