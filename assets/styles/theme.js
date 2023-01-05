9d1829c6ac602b9fa0560e2ba53f810c81504e8c
vpvm96 <vpvm96@gmail.com>
2023-01-05 22:31:15 +0900

    feat. 테마와 유틸 적용

--- assets/styles/theme.js
+++ assets/styles/theme.js
@@ -2,9 +2,11 @@ import { BLACK_COLOR, WHITE_COLOR, YELLOW_COLOR } from "./colors"
 
 export const lightTheme = {
   title: BLACK_COLOR,
+  modalBg: "#d2dae2",
 }
 
 export const darkTheme = {
   title: WHITE_COLOR,
   categoryTitle: YELLOW_COLOR,
+  modalBg: "#d2dae2",
 }
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,12 @@
+import { Dimensions } from "react-native"
+
 export const getImgPath = (path) => {
   return `https://image.tmdb.org/t/p/w500/${path}`
 }
+
+export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
+  Dimensions.get("window")
+
+export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g
+export const pwRegex =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
